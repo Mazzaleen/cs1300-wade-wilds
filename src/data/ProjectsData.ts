727701c297a69bdf5c991e374710233e1267736b
Mazzaleen <mazine_suliman@brown.edu>
2022-12-08 14:53:09 -0500

    push some pics

--- src/data/ProjectsData.ts
+++ src/data/ProjectsData.ts
@@ -1,10 +1,35 @@
 
-// import laundryimg from "src/assets/bakery.png";
+// import slai from "src/assets/SLAI.png";
+// import frag from "src/assets/fragantica.png";
+// import laundry from "src/assets/laundry.png";
+// import cinder from "src/assets/cinder.png";
+// import bakery from "src/assets/bakery.png";
+// import panda from "src/assets/panda.png";
+// import pokemon from "src/assets/pokemon.png";
+
+// const slai ="src/assets/SLAI.png";
+// const frag = "src/assets/fragantica.png";
+// const laundry = "src/assets/laundry.png";
+// const cinder = "src/assets/cinder.png";
+// const bakery = "src/assets/bakery.png";
+// const panda = "src/assets/panda.png";
+// const pokemon = "src/assets/pokemon.png";
+
+const slai ="https://github.com/Mazzaleen/cs1300-wade-wilds/blob/main/src/assets/Screen%20Shot%202022-12-08%20at%202.48.49%20PM.png?raw=true";
+const frag = "https://github.com/Mazzaleen/cs1300-wade-wilds/blob/main/src/assets/Screen%20Shot%202022-12-08%20at%202.51.39%20PM.png?raw=true"
+const laundry = "https://github.com/Mazzaleen/cs1300-wade-wilds/blob/main/src/assets/laundry.png?raw=true";
+const cinder = "https://github.com/Mazzaleen/cs1300-wade-wilds/blob/main/src/assets/cinder.png?raw=true";
+const bakery = "https://github.com/Mazzaleen/cs1300-wade-wilds/blob/main/src/assets/bakery.png?raw=true";
+const panda = "https://github.com/Mazzaleen/cs1300-wade-wilds/blob/main/src/assets/panda.png?raw=true";
+const pokemon = "https://github.com/Mazzaleen/cs1300-wade-wilds/blob/main/src/assets/pokemon.png?raw=true";
+
+
 
 interface ProjectsType {
   title: string
   description: string
-  image: string
+  image : string
+  // image: Object
   GitHub: string
   hosted: string
 }
@@ -14,7 +39,7 @@ export const ProjectsData: ProjectsType[] = [
   {
     title: 'SLAI AI',
     description: `Utilized Figma tools to redesign interactive User Interface for Slai, a startup that provides AI solutions for the insurance industry`,
-    image: 'src/assets/SLAI.png',
+    image: slai,
     GitHub: '',
     hosted: 'https://www.figma.com/proto/oIdJCkKCWP12fRjjfAyN9U/Slai-Hifi-Mockup?node-id=239%3A149&starting-point-node-id=239%3A149',
   },
@@ -22,7 +47,7 @@ export const ProjectsData: ProjectsType[] = [
   {
     title: 'Fragantica',
     description: `Utilized Figma, HTML, CSS, and JavaScript to redesign Fragantica's website`,
-    image: 'src/assets/fragantica.png',
+    image: frag,
     GitHub: 'https://github.com/8mzln8/Responsive-Redesign',
     hosted: 'https://8mzln8.github.io/Responsive-Redesign/index.html',
   },
@@ -30,7 +55,7 @@ export const ProjectsData: ProjectsType[] = [
   {
     title: 'Improving Laundry @ Brown',
     description: `Utilized HTM:5, CSS, and JavaScript to redesign the Brown Laundry Machine Interface`,
-    image: "src/assets/bakery.png",
+    image: laundry,
     GitHub: 'https://github.com/8mzln8/CSCI1300-Personas-Storyboarding',
     hosted: `https://8mzln8.github.io/CSCI1300-Personas-Storyboarding/`,
   },
@@ -38,7 +63,7 @@ export const ProjectsData: ProjectsType[] = [
   {
     title: 'Cinder: Tinder for CS Classes',
     description: `Utilized Figma, Adobe and Flutter to design a Tinder-like app for students to find computer science classes at Brown`,
-    image: 'assets/Cinder.png',
+    image: cinder,
     GitHub: '',
     hosted: 'https://www.figma.com/proto/fPEXAQZC55DWDLeRb2bE0U/Tinder-for-CS-Classes?node-id=1%3A3&starting-point-node-id=1%3A3',
   },
@@ -46,21 +71,22 @@ export const ProjectsData: ProjectsType[] = [
   {
     title: 'UX Research: Nested Bakery',
     description: `Designed Two interactive User Interfaces for Nested Bakery, For A/B testing purposes. Github link contains A, while hosted link contains B.`,
-    image: 'assets/bakery.png',
+    image: bakery,
     GitHub: 'https://www.figma.com/file/UOeTy0nUEqm8wPXHhsC30E/The-Nested-Bakery-Home-Page-Template-(Redesign-A)?node-id=0%3A1&t=t5GRK70BX92yQTbp-0',
     hosted: `https://www.figma.com/file/RJX6NcRxm4h9tXWzD5Bxoi/The-Nested-Bakery-Home-Page-Template-(Redesign-B)?t=t5GRK70BX92yQTbp-1`,
   },
   {
     title: 'Panda',
     description: `Single-Player/Multi-Player turn based Card-Game inspired by PANDA GAMES LTD. Utilized React,Java and Bootstrap to Host`,
-    image: 'assets/panda.png',
+    image: panda,
     GitHub: 'https://github.com/Mazzaleen/Panda-Card-Game',
     hosted: `https://www.figma.com/file/y6FxyVlCW27pOOFsfNyEGB/Panda?t=t5GRK70BX92yQTbp-0`,
   },
+
   {
     title: 'Pokemon Markeplace',
     description: `Utilized HTML, CSS, JS and Typescript to create a shopping cart for Pokemon`,
-    image: 'assets/pokemon.png',
+    image: pokemon,
     GitHub: 'https://github.com/Mazzaleen/ts-cs1300-deployment',
     hosted: `https://mazzaleen.github.io/ts-cs1300-deployment/`,
   }
