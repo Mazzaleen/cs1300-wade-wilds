103389ba0fa5ce8d2e3555642129553a1fdd4c8c
Mazzaleen <mazine_suliman@brown.edu>
2022-12-07 22:47:55 -0500

    change mazzaleen to github

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => {
  
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='' element={<Home />} />
         <Route path='/skills' element={<Skills />} />
         <Route path='/experience' element={<Experience />} />
         <Route path='/projects' element={<Projects />} />
